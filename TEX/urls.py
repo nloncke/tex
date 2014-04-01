5bd31768db0ca9b169f3c90c3d088ecdddc9b946
lx42 <lauraxu@princeton.edu>
2014-04-01 18:11:28 -0400

    starting to build backbone for search app

--- TEX/urls.py
+++ TEX/urls.py
@@ -8,7 +8,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'TEX.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'search.views.index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^helloDjango/', include('helloDjango.urls')),
+    url(r'^search/', include('search.urls')),
 )
 #test
--- search/views.py
+++ search/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from utils import *
+from django.shortcuts import render_to_response
 
-# Create your views here.
+# render(request, html template, function that returns dictionary)
+# render(-, nicole, jeffrey)
+def index(request):
+    return render_to_response('index.html')
+
+def results(request):
+    pass
+    #return render(request, 'search_results.html', hello())
+    
