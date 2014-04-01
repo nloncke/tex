5dab704cc9343042392178188d0785b37deaaec4
Ameera <aabdelaz@princeton.edu>
2014-04-01 16:18:42 -0400

    models.py in helloDjango has functions

--- helloDjango/models.py
+++ helloDjango/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Test(models.Model):
+    string = models.CharField(max_length=200)
+
+
+def putTest(s):
+    p = Test(string=s)
+    p.save()
+
+def getFirst():
+    t = Test.objects.get(id=1)
+    return t.string
