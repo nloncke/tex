83b39b15dafbac38cfaaa7a0868ca899b6a7e81e
Jeffrey Asala <jasala@princeton.edu>
2014-04-01 23:44:54 -0400

    Changed back to Cling, I needed to upgrade setup tools to fix that

--- TEX/wsgi.py
+++ TEX/wsgi.py
@@ -10,9 +10,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TEX.settings")
 
+# If this breaks for you, try upgrading setuptools
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
-
-# from dj_static import Cling
-# application = Cling(get_wsgi_application())
+from dj_static import Cling
+application = Cling(get_wsgi_application())
