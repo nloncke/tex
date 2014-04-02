3e307c69fa6cead842ebe37aa8eb79445187d304
Jeffrey Asala <jasala@princeton.edu>
2014-04-01 20:18:47 -0400

    Fixed static problem

--- TEX/settings.py
+++ TEX/settings.py
@@ -95,6 +95,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'webpages/css') 
+    , os.path.join(BASE_DIR, 'webpages/img')
+    , os.path.join(BASE_DIR, 'webpages/js') 
+    , os.path.join(BASE_DIR, 'webpages/less')
+    )
+
 
 # Email settings
 EMAIL_HOST = 'smtp.gmail.com'
