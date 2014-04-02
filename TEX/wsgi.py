c8320d2f301a89e14376531466f80b0601cd7494
Jeffrey Asala <jasala@princeton.edu>
2014-04-01 23:26:38 -0400

    Removing Cling in wsgi.py because breaks my local code

--- TEX/wsgi.py
+++ TEX/wsgi.py
@@ -11,6 +11,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TEX.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+application = get_wsgi_application()
 
-application = Cling(get_wsgi_application())
+
+# from dj_static import Cling
+# application = Cling(get_wsgi_application())
