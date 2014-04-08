819e6d6ae3886f668965629036bd1636f56d19bd
Jeffrey Asala <jasala@princeton.edu>
2014-04-08 14:36:01 -0400

    Utilizing the template structure of Django

--- buy/utils.py
+++ buy/utils.py
@@ -1,25 +1,26 @@
 from django.core.mail import send_mail, EmailMultiAlternatives
+from django.http import HttpResponse
 from TEX import settings
 import sys
 
 from_email =  settings.EMAIL_HOST_USER
 subject = settings.EMAIL_SUBJECT_PREFIX
 
-def notify_users(msg, addrs):
+def notify_users(addrs):
     ''' addrs is a list of email addresses'''
     #Send a simple 
-    send_mail(subject, msg, from_email, addrs)
+#     send_mail(subject, msg, from_email, addrs)
     
     #Send a formatted email with a backup in case HTML is no supported
     text_content = 'This is an important message.'
-    html_content = '<p>This is an <strong>important</strong> message.</p>'
+    html_content = HttpResponse('<p>This is an <strong>important</strong> message.</p>')
     msg = EmailMultiAlternatives(subject, text_content, from_email, addrs)
     msg.attach_alternative(html_content, "text/html")
     msg.send()
-    
+    print html_content
     
 if __name__ == "__main__" :
-    notify_users(sys.argv[1], [sys.argv[2]])
+    notify_users([sys.argv[1]])
     
 # Use
 # export DJANGO_SETTINGS_MODULE=TEX.settings
--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# No tests
+
+# Testing had problems with the database 
+# Didn't think it was worth my time to look into
+# fixing it
+
+# Jeff
+        
