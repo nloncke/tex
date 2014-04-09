9f0fc270b576579811623084d52f9e184de41e5d
lx42 <lauraxu@princeton.edu>
2014-04-09 15:06:19 -0400

    modifications to include jeffrey's changes

--- sell/views.py
+++ sell/views.py
@@ -1,26 +1,22 @@
 from django.shortcuts import render
 from sell.forms import SellForm
 from search.views import validate_isbn
-from search.models import *
+from utils import *
 result = {}
 def sell_form(request):   
     global result
     if request.method == 'POST':
         isbn = request.POST.get("target_isbn","0")
         if validate_isbn(isbn):
-            result = get_book_info(isbn, thumb=False)       
-            form = SellForm(request.POST)
-            if form.is_valid():
-               sell_data = form.cleaned_data
-               return HttpResponseRedirect('/sell/thanks/')
+            result = get_book_info(isbn)       
         else:
             # need an error html page
             return render(request, 'search_empty_prompt.html', {"query": isbn})
     else: 
         pass      
     form = SellForm()
-        
-    result["form"] = form
+    
+    result["form"] = form    
     return render(request, 'sell_form.html', result)
 
 def sell_confirm(request):
