3e5aa28623021c5c3d42be7633781069430a83e0
lx42 <lauraxu@princeton.edu>
2014-04-09 17:30:39 -0400

    more updates to sell app; created thanks page

--- sell/views.py
+++ sell/views.py
@@ -15,5 +15,22 @@ def sell_form(request):
     return render(request, 'sell_form.html', result)
        
 def sell_submit(request):
-    return render(request, 'sell_submit.html')
+    offer = {}
+    if request.method == 'POST':   
+        offer["price"] = request.POST.get("price", "0")
+        offer["picked_condition"] = request.POST.get("picked_condition", "0")
+        offer["description"] = request.POST.get("description", "0")
+        offer["seller_id"] = "TEMP"
+        offer["auction_id"] = "0"
+        '''if validate_offer(offer):
+            return render(request, 'sell_submit.html')
+        else:
+            # need an error html page
+            return render(request, 'search_empty_prompt.html', {"query": "temporary"})'''
+        return render(request, 'sell_submit.html')
 
+def validate_offer(offer):
+    if offer.price == "0"|offer.picked_condition == "0"|offer.description == "0"|offer.seller_id == "0":
+        return False
+    else:
+        return True
