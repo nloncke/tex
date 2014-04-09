c0f61547bd18854381387f0e2121a9a0b53eb184
Jeffrey Asala <jasala@princeton.edu>
2014-04-09 19:36:26 -0400

    adding put offer

--- sell/utils.py
+++ sell/utils.py
@@ -3,3 +3,7 @@ from search import models
 
 def get_book_info(isbn):
     return {"book":models.get_book_info(isbn = isbn, thumb=False)[0]}
+
+
+def put_offer(isbn, offer):
+    return sell.models.put_offer(isbn=isbn, offer=offer)
