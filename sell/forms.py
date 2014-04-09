e58e435cd44b059b54aff344f1b8a756318855c2
lx42 <lauraxu@princeton.edu>
2014-04-08 22:47:50 -0400

    working on sell form with test template (not included)

--- sell/forms.py
+++ sell/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 
 class SellForm(forms.Form):
-    auction = forms.BooleanField(intial=False, required=False)
+    #auction = forms.BooleanField(intial=False, required=False)
     CONDITIONS = (('New', 'New'),
                   ('Like New', 'Like New'),
                   ('Very Good', 'Very Good'),
                   ('Good', 'Good'),
                   ('Acceptable', 'Acceptable'),)
-    picked_condition = forms.MultipleChoiceField(choices=CONDITIONS, widget=forms.CheckboxInput())
+    picked_condition = forms.ChoiceField(choices=CONDITIONS, widget=forms.RadioSelect())
     description = forms.CharField(max_length=500, widget=forms.Textarea)
     #price = forms.CharField(regex=r"(^[1-9]$)|(^[1-9]\d$)|(^1\d\d$)|(^2\d\d$)")
     price = forms.IntegerField(min_value=1, max_value=200)
