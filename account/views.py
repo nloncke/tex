af082bd43b4a72fff8033ef89c9cc85e9d5fbf80
lx42 <lauraxu@princeton.edu>
2014-04-09 21:57:23 -0400

    hardcoded account view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from utils import *
+import re
 
-# Create your views here.
+# render(request, html template, function that returns dictionary)
+# render(-, nicole, jeffrey)
+def account_index(request):
+    return render(request,'account_index.html')
