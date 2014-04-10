04e67813ed14d86962f34dffb86b2d201145ac2f
Ameera <aabdelaz@princeton.edu>
2014-04-09 23:28:39 -0400

    implemented put-offer; added a table of users.

--- account/models.py
+++ account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    sell_list = models.CharField(max_length=500)
+    watch_list = models.CharField(max_length=500)
+    email = models.CharField(max_length=100)
--- sell/models.py
+++ sell/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from search.models import *
 
-# Create your models here.
+# put offer in table of offers, and update table of users
+def put_offer(offer, isbn, course):
+    # create offer
+    new_offer = Offer(isbn=isbn, seller = offer['seller_id'], price=offer['price'], course=course, condition=offer['condition'], description=offer['description'], auction_id=offer['auction_id']) 
+
+    # put in table of offers
+    new_offer.save()
+
+    # add offer_id to sell_list of seller
+    qset = User.objects.filter(id=offer['seller_id'])
+    for seller in qset:
+        seller.sell_list = seller.sell_list + ' ' + str(new_offer.id)
+    
