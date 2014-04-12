cbeccc1b494be2bd08b831af80fdf810cd535b25
lx42 <lauraxu@princeton.edu>
2014-04-12 15:03:03 -0400

    editing buy

--- buy/views.py
+++ buy/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils import *
+from models import *
 from book import utils
 import re
 
 def buy_confirmation(request):
     result = {}
     if request.method == "POST":
-        result["offer_id"] = request.POST.get("offer_id", "0")
-        result["buyer_id"] = request.POST.get("buyer_id", "0")
-        isbn = result["offer_id"].isbn
-        book = get_book(isbn)
-        result["book"] = book
+        offer_id = request.POST.get("offer_id", "0")
+        buyer_id = request.POST.get("buyer_id", "0")
+        sold_offer = get_offer(offer_id)
+        if sold_offer:
+            isbn = sold_offer["isbn"]
+            seller_id = sold_offer["seller_id"]
+            result = utils.get_book(isbn)
+            result["sold_offer"] = sold_offer
+            result["seller_id"] = seller_id
         return render(request, "buy_confirmation.html", result)
