5ff5484eb4ed724b2b2b0723a4f22e4979b22b25
lx42 <lauraxu@princeton.edu>
2014-04-12 11:55:00 -0400

    add to follow list

--- book/views.py
+++ book/views.py
@@ -16,3 +16,18 @@ def book_index(request):
     else:
         # need an error html page
         return render(request, 'search_empty_prompt.html', {"query": isbn})
+    
+def book_follow(request):
+    isbn = request.POST.get("target_isbn", "0")
+    follow_user_id = request.POST.get("follow_user_id", "0")
+    if validate_isbn(isbn):
+        add_to_follow(follow_user_id=follow_user_id, isbn=isbn)
+        result["isbn"] = isbn
+        result["follow_user_id"] = follow_user_id
+        return render(request, 'account_index', result)
+    
+    else:
+        pass
+        # error page
+        
+        
