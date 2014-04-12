2fcaf4b335e3ba3433c142256a680aad6f4ae4cb
Nicole Loncke <nloncke15@gmail.com>
2014-04-12 13:45:09 -0400

    still working on confirmation

---
+++ buy/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+from buy import views
+
+#url(regex and form action and website url, views.py name, nothing)
+#url(nicole and laura, laura, laura)
+urlpatterns = patterns('',
+    url(r'^$', views.buy_confirmation, name='buy_confirmation'),
+    url(r'^follow/$', views.book_follow, name='book_follow'),
+)
