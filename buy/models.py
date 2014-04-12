0aa23e7d84c5c6b8fd2002f018f3027c171ab039
Ameera <aabdelaz@princeton.edu>
2014-04-12 14:45:02 -0400

    made get_offer remove offer from database

--- buy/models.py
+++ buy/models.py
@@ -6,14 +6,14 @@ def get_offer(offer_id):
     qset = Offer.objects.filter(id=offer_id)
     if len(qset) > 0:
         # remove offer from seller field 
-        seller = BookUser.objects.filter(id=qset[0].seller_id)                                                                                                    
+        seller = BookUser.objects.filter(id=qset[0].seller)                                                                                                    
         if len(seller) > 0:
             tokens = seller[0].sell_list.split()
             tokens.remove(qset[0].isbn)
             ' '.join(tokens)
-
-        return { 'isbn': qset[0].isbn, 'seller_id':qset[0].seller }
-
+        d =  { 'isbn': qset[0].isbn, 'seller_id':qset[0].seller }
+        qset[0].delete()
+        return d
 
     else:
         return None
