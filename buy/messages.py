85e502092e6ec82b52324c64dcdb94d60482441d
Jeffrey Asala <jasala@princeton.edu>
2014-04-13 16:06:06 -0400

    Finally finished Email confirmation

---
+++ buy/messages.py
@@ -0,0 +1,19 @@
+TEXT_STUB = '%s has just purchased %s from %s for $%s . Please follow up with each other to seal the deal.'
+
+HTML_STUB = '<html>\
+<head>\
+</head>\
+<body>\
+   <div style= "display: inline-block; display: block; height: auto; width:600px; padding: 4px; font-family: sans-serif;\
+  margin-bottom: 20px; line-height: 1.428571429; background-color: #ffffff; border: 1px solid #dddddd; border-radius: 4px">\
+     <div style="overflow: hidden; zoom: 1">\
+         <img src="cid:frontcover" style="margin-top: 0; margin-right: 10px; float: left !important"\
+         alt='' />\
+           <h3 style="margin: 0 0 5px;">\
+             Transaction Complete\
+           </h3>%s\
+     </div>\
+   </div>\
+   Thank you for using tex!\
+</body>\
+</html>'
