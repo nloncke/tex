e3a18f8cc0ee37212f3515fa865d4f3a876bee58
lx42 <lauraxu@princeton.edu>
2014-04-14 15:37:38 -0400

    beginning to allow offer edits

--- sell/urls.py
+++ sell/urls.py
@@ -7,4 +7,5 @@ from sell import views
 urlpatterns = patterns('',
     url(r'^$', views.sell_form, name='sell_form'),
     url(r'^submit/$', views.sell_submit, name='sell_form'),
+    url(r'^edit/$', views.sell_edit, name='sell_form'),
 )
--- sell/views.py
+++ sell/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from search.views import validate_isbn
 from utils import *
+from book.utils import *
+from buy.models import *
 
 def sell_form(request):   
     if request.method == 'POST':
@@ -19,6 +21,7 @@ def sell_form(request):
        
 def sell_submit(request):
     offer = {}
+    result = {}
     if request.method == 'POST':   
         offer["isbn"] = request.POST.get("target_isbn", "0")
         offer["course"] = request.POST.get("course", "0")
@@ -27,17 +30,25 @@ def sell_submit(request):
         offer["description"] = request.POST.get("description", "0")
         offer["seller_id"] = 10
         offer["auction_id"] = "0"
-        put_offer(offer["isbn"], offer)
+        result["offer_id"] = put_offer(offer["isbn"], offer)
         '''if validate_offer(offer) and validate_isbn(isbn):
             put_offer(offer)
             return render(request, 'sell_submit.html')
         else:
             # need an error html page
             return render(request, 'search_empty_prompt.html', {"query": "temporary"})'''
-        return render(request, 'sell_submit.html')
+        return render(request, 'sell_submit.html', result)
     else:
         return render(request, "error_page.html")
     
+def sell_edit(request):
+    result = {}
+    if request.method == 'POST':  
+        offerid = request.POST.get("offer_id", "0")
+        edit_offer = get_offer(offerid)
+        book_isbn = edit_offer["isbn"]
+        result["offer"] = get_book(book_isbn)       
+    return render(request, "sell_form_edit.html", result)
 
 def validate_offer(offer):
     if offer["price"] == "0"|offer["condition"] == "0"|offer["description"] == "0"|offer["seller_id"] == "0":
