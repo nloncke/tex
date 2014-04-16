696b9261d008275d00471d926adf98431edce042
Ameera <aabdelaz@princeton.edu>
2014-04-16 18:43:42 -0400

    did follow and unfollow

--- account/models.py
+++ account/models.py
@@ -17,6 +17,22 @@ class BookUser(models.Model):
 def get_seller_offers(seller_id):
     return Offer.objects.filter(seller_id=seller_id)
 
+def follow(user, isbn):
+    bu = user.bookuser
+    if bu.watch_list == '':
+        bu.watch_list = isbn
+    else:
+        bu.watch_list = bu.watch_list + ' ' + isbn
+    bu.save()
+
+def unfollow(user, isbn):
+    bu = user.bookuser
+    tokens = bu.watch_list.split()
+    if isbn in tokens:
+        tokens.remove(isbn)
+    bu.watch_list = ' '.join(tokens)
+    bu.save()
+
 class PopulatedCASBackend(CASBackend):
     """CAS authentication backend with user data populated from AD"""
 
