181f6aea4281cfd1c743fb06b861279e502e8755
Jeffrey Asala <jasala@princeton.edu>
2014-04-16 12:55:01 -0400

    Return time left

--- book/utils.py
+++ book/utils.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+from time import gmtime 
 from search.models import *
 import sys
 
@@ -5,6 +7,10 @@ AMAZON_STUB = "http://www.amazon.com/gp/search/ref=sr_adv_b/?search-alias=stripb
 CHEGG_STUB = "http://www.chegg.com/search/%s"
 CAMPUS_STUB = "http://www.campusbooks.com/books/search.php?search_type=single&isbn=%s"
 LABYRINTH_STUB = "http://www.labyrinthbooks.com/all_search.aspx?sisbn=%s"
+
+
+def get_time_left(auction, timenow):
+    auction["time_left"] = auction["end_time"] - timenow
 
 
 def get_book(isbn):
@@ -21,11 +27,18 @@ def get_book(isbn):
     book_info["chegg"] = CHEGG_STUB % isbn
     book_info["campus"] = CAMPUS_STUB % isbn
     book_info["labyrinth"] = LABYRINTH_STUB % isbn
-    
+
     result = {"book":book_info}
+    
+    result["auctions"] = sorted(get_auctions(isbn), key=(lambda x:x["current_price"]))
+    # We will have to deal with timezon problems later
+    timenow = gmtime()
+    #find time left
+    map(lambda x:get_time_left(x, timenow), result["auctions"])
+    
+    
     result["offers"] = sorted(get_offers(isbn), key=(lambda x:x["buy_price"]))
     return result
-
 
 if __name__ == "__main__" :
     print get_book(sys.argv[1])
