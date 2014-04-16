76b40f69c0b618627c9afa2b3a727dd7dfda1ffa
Ameera <aabdelaz@princeton.edu>
2014-04-16 17:41:41 -0400

    implemented get_seller_offers in account app

--- account/models.py
+++ account/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django_cas.backends import CASBackend
+from search.models import *
 
 # Create your models here.
 class BookUser(models.Model):
@@ -13,6 +14,9 @@ class BookUser(models.Model):
     def __unicode__(self):
         return self.user.username
 
+# returns list of offers of seller with the given id
+def get_seller_offers(seller_id):
+    return Offer.objects.filter(seller_id=seller_id)
 
 class PopulatedCASBackend(CASBackend):
     """CAS authentication backend with user data populated from AD"""
