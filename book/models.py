972775820e607100c312ef293b2a6b6b61b3376e
Ameera <aabdelaz@princeton.edu>
2014-04-18 22:42:28 -0400

    filled in get_followers in book

--- book/models.py
+++ book/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from account.models import *
 
 # Create your models here.
 
@@ -6,9 +7,11 @@ def get_followers(isbn):
     ''' Return a list of user_id of all user that are following
     this book 
     '''
-    
-    # dummy for testing 
-    return ["aabdelaz", "lauraxu", "jasala", "nloncke"]
+    regex = '.*' + isbn + '.*'
+    qset = BookUser.objects.filter(watch_list__iregex=regex)
 
-
-
+    followers = []
+    for object in qset:
+       	followers.append(object.user.username)
+		
+    return followers
