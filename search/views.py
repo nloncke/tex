1e4a1d8f5b32033379135796b87016e1122f05a2
lx42 <lauraxu@princeton.edu>
2014-04-18 22:17:54 -0400

    moved validate functions to utils

--- search/views.py
+++ search/views.py
@@ -67,73 +67,7 @@ def course(request):
     else:
         return render(request, 'search_empty_prompt.html', {"query": course})
         
-def validate_isbn(isbn):
-    regex = re.compile("(^((\s)*([0-9]-?){9}[0-9Xx](\s)*)$)|(^((\s)*(97[89]-?([0-9]-?){9}[0-9])(\s)*)$)")
-    if regex.search(isbn):
-        # Remove non ISBN digits, then split into an array
-        chars = list(str(re.sub("[^0-9Xx]", "", isbn)))
-        # Remove the final ISBN digit from `chars`, and assign it to `last`
-        last  = chars.pop()  
-        if len(chars) == 9:
-        # Compute the ISBN-10 check digit
-            val = sum((x + 2) * int(y) for x,y in enumerate(reversed(chars)))
-            check = 11 - (val % 11)
-            if check == 10:
-                check = "X"
-            elif check == 11:
-                check = "0"
-        else:
-        # Compute the ISBN-13 check digit
-            val = sum((x % 2 * 2 + 1) * int(y) for x,y in enumerate(chars))
-            check = 10 - (val % 10)
-            if check == 10:
-                check = "0"    
-        if (str(check) == last):
-            #print "Valid ISBN"
-            return True
-        else:
-            #print "Invalid ISBN check digit"
-            return False
-    else:
-        #print "Invalid ISBN"
-        return False
 
-def validate_title(title):
-    regex = re.compile("^[\w\s]{1,200}$")
-    if re.search(regex, title):
-        return True
-    else:
-        return False
-        
-def validate_author(author):
-    regex = re.compile("^[a-zA-Z\s]{1,100}$")
-    if re.search(regex, author):
-        return True
-    else:
-        return False
-
-def validate_course(course):
-    regex = re.compile("(\s)*[a-zA-Z]{3}( )*[0-9]{3}(\s)*$")
-    if re.search(regex, course):
-        return True
-    else:
-        return False
-        
-def convert_to_13(isbn):
-    if len(isbn) == 13:
-        return isbn
-    if len(isbn) == 10:
-        chars = '978'
-        chars = list(chars + isbn)
-        chars.pop()
-        val = sum((x % 2 * 2 + 1) * int(y) for x,y in enumerate(chars))
-        check = 10 - (val % 10)
-        if check == 10:
-            check = "0"
-        chars.append(str(check))
-        return ''.join(chars)
-    else:
-        return 0
         
 ##########################################################  
 
