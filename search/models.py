20e869fa24c6ec7dcf8f86a9fdae84ed97c86cb2
Ameera <aabdelaz@princeton.edu>
2014-04-20 12:16:34 -0400

    added binaryfield stuffs

--- search/models.py
+++ search/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os.path
 
 # Create your models here.
 class Offer(models.Model):
@@ -28,8 +29,10 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     frontcover = models.CharField(max_length=100)
+	coverbytes = models.BinaryField()
     thumbnail = models.CharField(max_length=100)
-    pub_date = models.CharField(max_length=100) 
+	thumbbytes = models.BinaryField()
+	pub_date = models.CharField(max_length=100) 
     def __repr__(self):
         s = self.isbn + ' ' + self.title + ' ' + self.author + ' ' + self.frontcover + ' ' + self.thumbnail
         return s
@@ -59,13 +62,27 @@ def get_book_info(isbn = None, title = None, author = None, course = None, thumb
             newqset = qset.filter(isbn=object)
             for newob in newqset:
                 if thumb == True:
+					if not os.path.isfile(newob.thumbnail):
+						with open(newob.thumbnail, 'wb') as f:
+							f.write(newob.thumbbytes)
                     books.append({'isbn':newob.isbn, 'title':newob.title, 'author':newob.author, 'pub_date':newob.pub_date, 'thumbnail':newob.thumbnail})
                 else:
+					if not os.path.isfile(newob.frontcover):
+						with open(newob.frontcover, 'wb') as f:
+							f.write(newob.coverbytes)
                     books.append({'isbn':newob.isbn, 'title':newob.title, 'author':newob.author, 'pub_date':newob.pub_date, 'frontcover':newob.frontcover})
 
     elif (thumb == True):
+		if object in qset:
+			if not os.path.isfile(object.thumbnail):
+				with open(object.thumbnail, 'wb') as f:
+					f.write(object.thumbbytes)
         books = [{'isbn':object.isbn, 'title':object.title, 'author':object.author, 'thumbnail':object.thumbnail,'pub_date':object.pub_date} for object in qset]
     else:
+		if object in qset:
+			if not os.path.isfile(object.frontcover):
+				with open(object.frontcover, 'wb') as f:
+					f.write(object.coverbytes)
         books = [{'isbn':object.isbn, 'title':object.title, 'author':object.author, 'frontcover':object.frontcover, 'pub_date':object.pub_date} for object in qset]
 
     return books
@@ -78,6 +95,10 @@ def get_course_list(course):
     return set(isbns)
 
 def update_book_cache(**book_info):
+	with open(book_info['frontcover'], 'r') as f:
+...     book_info['coverbytes'] = f.read()
+	with open(book_info['thumbbytes'], 'r') as f:
+		book_info['thumbbytes'] = f.read()
     book = Book(**book_info)
     book.save()
 
