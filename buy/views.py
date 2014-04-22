9c7a1846053129465bb7d20876628bf0f724b347
lx42 <lauraxu@princeton.edu>
2014-04-21 20:33:52 -0400

    can remove auction form account page buy now is working

--- buy/views.py
+++ buy/views.py
@@ -8,15 +8,30 @@ from book.utils import *
 def buy_confirmation(request):
     result = {}
     if request.method == "POST":
-        offer_id = request.POST.get("offer_id", "0")
-        buyer_id = request.user.username
-        sold_offer = remove_offer(offer_id)
-        if sold_offer:
-            isbn = sold_offer["isbn"]
-            seller_id = sold_offer["seller_id"]
-            result = get_book(isbn)
-            result["sold_offer"] = sold_offer
-            result["seller_id"] = seller_id
+        is_auction = request.POST.get("is_auction","")
+        if is_auction:
+            from models import remove_auction
+            result = {}
+            if request.method == "POST":
+                auction_id = request.POST.get("auction_id","0")
+                buyer_id = request.user.username
+                sold_auction = remove_auction(auction_id, True)
+                if sold_auction:
+                    isbn = sold_auction["isbn"]
+                    seller_id = sold_auction["seller_id"]
+                    result = get_book(isbn)
+                    result["sold_offer"] = sold_auction
+                    result["seller_id"] = seller_id
+        else:
+            offer_id = request.POST.get("offer_id", "0")
+            buyer_id = request.user.username
+            sold_offer = remove_offer(offer_id)
+            if sold_offer:
+                isbn = sold_offer["isbn"]
+                seller_id = sold_offer["seller_id"]
+                result = get_book(isbn)
+                result["sold_offer"] = sold_offer
+                result["seller_id"] = seller_id
         return render(request, "buy_confirmation.html", result)
     return render(request, 'error_page.html')
 
