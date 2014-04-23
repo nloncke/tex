f28245feb69728c0ed6c8c3b746803ec253fade6
lx42 <lauraxu@princeton.edu>
2014-04-22 22:25:35 -0400

    changed default countdown time

--- webpages/static/jquery.countdown.js
+++ webpages/static/jquery.countdown.js
@@ -111,7 +111,7 @@ and GPL-3.0 (http://opensource.org/licenses/GPL-3.0) licenses.
       return this.init();
     };
     $.countdown.defaultOptions = {
-      date: "June 7, 2087 15:03:25",
+      date: "October 3, 2093 13:24:21",
       refresh: 1000,
       onEnd: $.noop,
       render: function(date) {
