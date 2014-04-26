150abf8899997c6f43e1f509ea0e5237870d5333
lx42 <lauraxu@princeton.edu>
2014-04-26 02:10:02 -0400

    merge conflicts

--- buy/views.py
+++ buy/views.py
@@ -53,17 +53,10 @@ def bid(request):
         bid = request.POST.get("bid", "0")
         new_bid = int(current_price) + int(bid)
         new_current_price = bid_auction(auction_id=auction_id, current_price=new_bid, buyer_id=buyer_id)
-        
-<<<<<<< HEAD
-        isbn = get_auction_isbn(auction_id=auction_id)
-        result = get_book(isbn=isbn)
-=======
         if new_current_price == -1:
             return render(request, 'nocheating.html', {'buyer_id':buyer_id})
-        
-        isbn = get_auction_isbn(auction_id)
-        result = get_book(isbn)
->>>>>>> 7e8764730b3da59bbdc250ff8a6c6f3b41bcee4a
+        isbn = get_auction_isbn(auction_id=auction_id)
+        result = get_book(isbn=isbn)
         
         result["current_price"] = str(new_current_price)
         
