26238e0b0c66ca0aa83aff7a7ee8526e52f68168
Jeffrey Asala <jasala@princeton.edu>
2014-05-06 20:59:07 -0400

    Commit my unknown changes

--- search/utils.py
+++ search/utils.py
@@ -94,7 +94,8 @@ def fetch_isbn(isbn):
     return info
 
 def fetch_isbn_amazon(isbn):
-    url = create_aws_request(isbn)
+    # url = create_aws_request(isbn)
+    url = 'http://www.google.com'
     try:
         response = urllib2.urlopen(url)
         tree = ET.parse(response)
