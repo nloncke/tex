d18567d5363b2066201a69a57339d150079d0cd8
Jeffrey Asala <jasala@princeton.edu>
2014-05-13 14:49:25 -0400

    Pushing to heroku

--- TEX/settings.py
+++ TEX/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'xqz)1%#98!jgi*pimqwut^sl4tf(i@c*q32)z$djde=$@1&gfw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
-# DEBUG = False
+# DEBUG = True 
+DEBUG = False
 
 TEMPLATE_DEBUG = DEBUG
 
@@ -129,16 +129,16 @@ DATABASES = {'default': dj_database_url.config()}
 # https://docs.djangoproject.com/en/1.6/ref/settings/
 #databases
 #        
-DATABASES = {
-    'default': {
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydatabase',
-        'USER': 'tex',
-        'PASSWORD': 'axal@tex',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE':'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'mydatabase',
+#         'USER': 'tex',
+#         'PASSWORD': 'axal@tex',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
